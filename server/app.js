2d64ad870d664c7a46ed3317391e846b05c487d5
moses ka <you@example.com>
2023-08-16 20:43:07 +0200

    allow the api to fitch

--- server/app.js
+++ server/app.js
@@ -2,6 +2,11 @@ import express from 'express'
 import dotenv from 'dotenv'
 dotenv.config()
 const app = express()
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
+    });
 app.use(express.json())
 import route from './router.js'
 const port = process.env.PORT || 3013
